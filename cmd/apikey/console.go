ed7e30a51a046939c0a14fe6d1fd79593ae7c3d4
yinyin <yinyin2010@gmail.com>
2019-05-29 16:36:50 +0800

    add apikey gen tool

---
+++ cmd/apikey/console.go
@@ -0,0 +1,272 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/skyrings/skyring-common/tools/uuid"
+	"github.com/zeebo/errs"
+
+	"storj.io/storj/satellite/console/consoleauth"
+)
+
+func graphqlDo(client *http.Client, request *http.Request, jsonResponse interface{}) error {
+	resp, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err = resp.Body.Close()
+	}()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var response struct {
+		Data   json.RawMessage
+		Errors []interface{}
+	}
+
+	if err = json.NewDecoder(bytes.NewReader(b)).Decode(&response); err != nil {
+		return err
+	}
+
+	if response.Errors != nil {
+		return errs.New("inner graphql error")
+	}
+
+	if jsonResponse == nil {
+		return nil
+	}
+
+	return json.NewDecoder(bytes.NewReader(response.Data)).Decode(jsonResponse)
+}
+
+func generateActivationKey(userID uuid.UUID, email string, createdAt time.Time) (string, error) {
+	claims := consoleauth.Claims{
+		ID:         userID,
+		Email:      email,
+		Expiration: createdAt.Add(24 * time.Hour),
+	}
+
+	// TODO: change it in future, when satellite/console secret will be changed
+	signer := &consoleauth.Hmac{Secret: []byte("my-suppa-secret-key")}
+
+	json, err := claims.JSON()
+	if err != nil {
+		return "", err
+	}
+
+	token := consoleauth.Token{Payload: json}
+	encoded := base64.URLEncoding.EncodeToString(token.Payload)
+
+	signature, err := signer.Sign([]byte(encoded))
+	if err != nil {
+		return "", err
+	}
+
+	token.Signature = signature
+
+	return token.String(), nil
+}
+
+func addProjectWithKey(key *string, createRegistrationTokenAddress, activationAddress, address string, param *Param) error {
+	client := http.Client{}
+
+	var createTokenResponse struct {
+		Secret string
+		Error  string
+	}
+	{
+		request, err := http.NewRequest(
+			http.MethodGet,
+			createRegistrationTokenAddress,
+			nil)
+		if err != nil {
+			return err
+		}
+		request.Header.Set("Authorization", param.Token)
+
+		resp, err := client.Do(request)
+		if err != nil {
+			return err
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+
+		if err = json.NewDecoder(bytes.NewReader(b)).Decode(&createTokenResponse); err != nil {
+			return err
+		}
+
+		if createTokenResponse.Error != "" {
+			return errs.New(createTokenResponse.Error)
+		}
+
+		err = resp.Body.Close()
+		if err != nil {
+			return err
+		}
+	}
+
+	// create user
+	var user struct {
+		CreateUser struct {
+			Email     string
+			CreatedAt time.Time
+			ID        string
+		}
+	}
+	{
+		createUserQuery := fmt.Sprintf(
+			"mutation {createUser(input:{email:\"%s\",password:\"%s\",fullName:\"%s\", shortName:\"\"}, secret:\"%s\" ){id,email,createdAt}}",
+			param.UserEmail,
+			param.UserPassword,
+			param.UserFullName,
+			createTokenResponse.Secret)
+
+		request, err := http.NewRequest(
+			http.MethodPost,
+			address,
+			bytes.NewReader([]byte(createUserQuery)))
+		if err != nil {
+			return err
+		}
+
+		request.Header.Add("Content-Type", "application/graphql")
+
+		err = graphqlDo(&client, request, &user)
+		if err != nil {
+			return err
+		}
+	}
+
+	var token struct {
+		Token struct {
+			Token string
+		}
+	}
+	{
+		userID, err := uuid.Parse(user.CreateUser.ID)
+		if err != nil {
+			return err
+		}
+
+		activationToken, err := generateActivationKey(*userID, user.CreateUser.Email, user.CreateUser.CreatedAt)
+		if err != nil {
+			return err
+		}
+
+		request, err := http.NewRequest(
+			http.MethodGet,
+			activationAddress+activationToken,
+			nil)
+		if err != nil {
+			return err
+		}
+
+		resp, err := client.Do(request)
+		if err != nil {
+			return err
+		}
+
+		err = resp.Body.Close()
+		if err != nil {
+			return err
+		}
+
+		tokenQuery := fmt.Sprintf(
+			"query {token(email:\"%s\",password:\"%s\"){token}}",
+			param.UserEmail,
+			param.UserPassword)
+
+		request, err = http.NewRequest(
+			http.MethodPost,
+			address,
+			bytes.NewReader([]byte(tokenQuery)))
+
+		if err != nil {
+			return err
+		}
+
+		request.Header.Add("Content-Type", "application/graphql")
+
+		err = graphqlDo(&client, request, &token)
+		if err != nil {
+			return err
+		}
+	}
+
+	// create project
+	var createProject struct {
+		CreateProject struct {
+			ID string
+		}
+	}
+	{
+		createProjectQuery := fmt.Sprintf(
+			"mutation {createProject(input:{name:\"%s\",description:\"\"}){id}}",
+			param.APIKeyName)
+
+		request, err := http.NewRequest(
+			http.MethodPost,
+			address,
+			bytes.NewReader([]byte(createProjectQuery)))
+
+		if err != nil {
+			return err
+		}
+
+		request.Header.Add("Content-Type", "application/graphql")
+		request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token.Token.Token))
+
+		if err := graphqlDo(&client, request, &createProject); err != nil {
+			return err
+		}
+	}
+
+	// create api key
+	var createAPIKey struct {
+		CreateAPIKey struct {
+			Key string
+		}
+	}
+	{
+		createAPIKeyQuery := fmt.Sprintf(
+			"mutation {createAPIKey(projectID:\"%s\",name:\"%s\"){key}}",
+			createProject.CreateProject.ID,
+			param.APIKeyName)
+
+		request, err := http.NewRequest(
+			http.MethodPost,
+			address,
+			bytes.NewReader([]byte(createAPIKeyQuery)))
+
+		if err != nil {
+			return err
+		}
+
+		request.Header.Add("Content-Type", "application/graphql")
+		request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token.Token.Token))
+
+		if err := graphqlDo(&client, request, &createAPIKey); err != nil {
+			return err
+		}
+	}
+
+	// return key to the caller
+	*key = createAPIKey.CreateAPIKey.Key
+	return nil
+}
---
+++ cmd/apikey/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	Host                           = "http://127.0.0.1:8081"
+	CreateRegistrationTokenAddress = Host + "/registrationToken?projectsLimit=1"
+	ConsoleActivationAddress       = Host + "/activation/?token="
+	ConsoleAPIAddress              = Host + "/api/graphql/v0"
+)
+
+type Param struct {
+	Token         string
+	UserEmail     string
+	UserPassword  string
+	UserFullName  string
+	UserShortName string
+	ProjectName   string
+	APIKeyName    string
+}
+
+var (
+	p = &Param{
+		Token:         "secure_token",
+		UserEmail:     "admin@bonuscloud.io",
+		UserPassword:  "qwer1234",
+		UserShortName: "store",
+		UserFullName:  "storj store",
+		ProjectName:   "store",
+		APIKeyName:    "TestProject",
+	}
+)
+
+func main() {
+	var err error
+	var apiKey string
+
+	if err = addProjectWithKey(&apiKey, CreateRegistrationTokenAddress, ConsoleActivationAddress, ConsoleAPIAddress, p); err != nil {
+		fmt.Println("create project fail, err=%s", err)
+	} else {
+		fmt.Printf("create projcet succ, apiKey=%s\n", apiKey)
+	}
+}
